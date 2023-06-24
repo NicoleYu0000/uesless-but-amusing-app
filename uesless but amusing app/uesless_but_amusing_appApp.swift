4ce2b76b75b05ba0308d4fc1aa1b296b7bacbeef
NicoleYu0000 <littleco6688@gmail.com>
2023-06-24 12:12:38 +0800

    Initial Commit

---
+++ uesless but amusing app/uesless_but_amusing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  uesless_but_amusing_appApp.swift
+//  uesless but amusing app
+//
+//  Created by Nicole Yu on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct uesless_but_amusing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
