6fa484448dba7d69bdea6ec77d0bab1786ad337d
NicoleYu0000 <littleco6688@gmail.com>
2023-07-01 10:50:04 +0800

    changes

--- uesless but amusing app/NextPageView.swift
+++ uesless but amusing app/NextPageView.swift
@@ -13,13 +13,24 @@ struct NextPageView: View {
     var body: some View {
         NavigationView {
             VStack {
+                
+                Text("'Always nurture your inner childhood dreams, life will become beautiful' \n - Invajy")
+                    .multilineTextAlignment(.center)
+                    .bold()
+                    .background(.yellow)
+                    .foregroundColor(.blue)
+                    .underline()
+                
+                    .font(.largeTitle)
+                
                 Button {
                     print("Pressed")
                     self.showView = true
                 } label: {
                     Text("Press for your dreams")
+                        .font(.largeTitle)
                         .padding()
-                        .frame(width:350, height: 50)
+                        .frame(width:350, height: 100)
                         .foregroundColor(.white)
                         .background(AngularGradient(colors: [.blue, .mint, .cyan, .blue], center: .center))
                         .cornerRadius(100)
